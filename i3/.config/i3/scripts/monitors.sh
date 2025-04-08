7bf112d14f5c4a5deeba5ee10cb545c8e06a0e07
Vuk Knezevic <kuvk.me@gmail.com>
2025-04-08 13:58:09 +0200

    Update

--- i3/.config/i3/scripts/monitors.sh
+++ i3/.config/i3/scripts/monitors.sh
@@ -11,5 +11,11 @@
 # fi
 #
 # desktop
-xrandr --auto --output HDMI-0 --primary --mode 1920x1080
-feh --bg-fill ~/wallpapers/wallpaper.jpg
+if xrandr | grep " connected " | awk '{ print$1 }' | grep HDMI-0; then
+    xrandr --auto --output DP-0 --primary --mode 2560x1440
+    xrandr --auto --output HDMI-0 --mode 1920x1080 --right-of DP-0
+    feh --bg-fill ~/wallpapers/wallpaper.jpg
+else
+    xrandr --auto --output DP-0 --mode 2560x1440
+    feh --bg-fill ~/wallpapers/wallpaper.jpg
+fi
