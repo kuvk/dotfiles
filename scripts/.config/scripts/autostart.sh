252b94783056af2a50154f4a7ffec6e23dd4e18d
Vuk Knezevic <kuvk.me@gmail.com>
2025-05-31 21:07:13 +0200

    Removed swayidle, setup hypridle, added media-inhibit.sh to start on login

--- scripts/.config/scripts/autostart.sh
+++ scripts/.config/scripts/autostart.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-IDLE="$HOME/.config/scripts/idle.sh"
-
+# RGB
 if command -v openrgb >/dev/null; then
     openrgb --startminimized -p vuk &
 fi
 
-if command -v swayidle >/dev/null; then
-    swayidle -w \
-        timeout 290 'notify-send "Idle" "Locking in 10 seconds..."' \
-        timeout 300 "if $IDLE; then pidof hyprlock || (hyprlock &); fi" \
-        timeout 1800 "if $IDLE; then pidof hyprlock || (hyprlock &); sleep 0.1; systemctl suspend; fi"
-fi
 exit 0
